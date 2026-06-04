f642d0f94e76b461733b6532437f1345152304c1
Aeon <99080267+Aaaaaaeon@users.noreply.github.com>
2026-06-04 09:03:37 +0200

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,3 +1,5 @@
-window.CODEREVIEW_CONFIG = {
+window.MMI_CONFIG = {
+    provider: "mistral-api",
     mistralApiKey: "VOTRE_CLE_API_ICI",
+    ollamaEndpoint: "http://localhost:11434/api/generate",
 };
